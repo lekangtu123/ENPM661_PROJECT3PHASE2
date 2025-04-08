2585c3c04dfc4db089155aa1e1114a15018a508f
lekangtu123 <lekangtu@gmail.com>
2025-04-08 10:18:40 -0700

    delete colcon build files

--- project3_ws/build/turtlebot3_project3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leoo/Desktop/proj3p2_tu_Li_ajayi/project3_ws/src/turtlebot3_project3
-# Build directory: /home/leoo/Desktop/proj3p2_tu_Li_ajayi/project3_ws/build/turtlebot3_project3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
