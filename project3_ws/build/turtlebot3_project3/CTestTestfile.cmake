ad08e4c35f8573e95e86718097d5ad59ae48799a
lekangtu123 <lekangtu@gmail.com>
2025-04-08 10:08:50 -0700

    part1

---
+++ project3_ws/build/turtlebot3_project3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leoo/Desktop/proj3p2_tu_Li_ajayi/project3_ws/src/turtlebot3_project3
+# Build directory: /home/leoo/Desktop/proj3p2_tu_Li_ajayi/project3_ws/build/turtlebot3_project3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
